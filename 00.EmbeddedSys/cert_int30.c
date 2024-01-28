c9628331d810eb3441de7f716fe48073b8d50fff
liulei <878923596@qq.com>
2024-01-28 14:42:54 +0800

    新增文件

---
+++ 00.EmbeddedSys/cert_int30.c
@@ -0,0 +1,5 @@
+//
+// Created by liulei on 2023/6/18.
+//
+
+#include "cert_int30.h"
---
+++ 00.EmbeddedSys/cert_int30.h
@@ -0,0 +1,8 @@
+//
+// Created by liulei on 2023/6/18.
+//
+
+#ifndef CPPSTUDY_CERT_INT30_H
+#define CPPSTUDY_CERT_INT30_H
+
+#endif //CPPSTUDY_CERT_INT30_H
